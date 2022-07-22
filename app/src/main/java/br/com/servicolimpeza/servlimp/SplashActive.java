ff509717f832d468393104af96d0f6037f12b72c
KaykeSAndrade <kaykewwe1212@hotmail.com>
2022-07-22 10:53:07 -0300

    initial commit

---
+++ app/src/main/java/br/com/servicolimpeza/servlimp/SplashActive.java
@@ -0,0 +1,14 @@
+package br.com.servicolimpeza.servlimp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+    }
+}
